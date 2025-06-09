8bdca378bbd7243cf25333186788e1679a209eea
sakurakotubaki <gitonly543@gmail.com>
2025-06-09 16:24:45 +0900

    refactor: select

--- lib/view/next_view.dart
+++ lib/view/next_view.dart
@@ -8,12 +8,17 @@ class NextView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final notes = ref.watch(notesViewModelProvider);
+    final selectValue = ref.watch(
+      notesViewModelProvider.select((value) => value),
+    );
 
     return Scaffold(
       appBar: AppBar(title: const Text('Next View')),
       body: Center(
         child: ListView(
           children: [
+            // selectValue
+            Text('Selected Value: ${selectValue.body}'),
             Text('ID: ${notes.id}'),
             Text('Body: ${notes.body}'),
             Text('Created At: ${notes.createdAt}'),
